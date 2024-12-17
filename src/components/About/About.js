201b06a33528b0cba320560a3fde0c068870296c
Abhishek <abhishekbhade2002@gmail.com>
2024-12-17 22:42:52 +0530

    updated About Us page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-    return (
-        <div>
-            <h1>About Page</h1>
-            <p>This is the about page content.</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+};
 
 export default About;
